68cbd8487076fcc570df26d0e09517f86772ac4b
B0295868 <shaurya.pratapsingh@airtel.com>
2024-04-20 12:09:10 +0530

    image generation funciton added

--- index.py
+++ index.py
@@ -1,3 +1,12 @@
-from src.together import Together
+import os
+from together import Together
 
-Together.fine_tuning.create
+client = Together(api_key=os.environ.get("TOGETHER_API_KEY"))
+
+response = client.images.generate(
+    prompt="space robots",
+    model="stabilityai/stable-diffusion-xl-base-1.0",
+    steps=10,
+    n=4,
+)
+print(response.data[0].b64_json)
